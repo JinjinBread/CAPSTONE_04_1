ce9291154f4f639cb58967c1fd0b6217e67c3b71
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-01-02 15:25:25 +0900

    init

---
+++ src/main/java/univcapstone/employmentsite/EmploymentsiteApplication.java
@@ -0,0 +1,13 @@
+package univcapstone.employmentsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentsiteApplication.class, args);
+	}
+
+}
