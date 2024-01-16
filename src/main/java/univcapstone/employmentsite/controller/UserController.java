4d39b2cfbd4f0a9572c13fe2dcdee914dec6b9c9
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-01-16 14:44:59 +0900

    feat: 이메일 인증 서비스 구현

--- src/main/java/univcapstone/employmentsite/controller/UserController.java
+++ src/main/java/univcapstone/employmentsite/controller/UserController.java
@@ -31,12 +31,6 @@ public class UserController {
         return "saved ok";
     }
 
-    @PostMapping("/confirm/email")
-    public String confirmEmail(@RequestParam("email") String email) {
-        //이메일 인증에 대한 로직
-        return "redirect:/jobhak.univ"; // 이메일 인중 성공 후 메인 페이지로 리다이렉트
-    }
-
     @PostMapping("/verify/id")
     public String verifyID(@RequestParam("id") String id) {
         //아이디 중복 확인에 대한 로직
