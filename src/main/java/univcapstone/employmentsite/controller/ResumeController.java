f2095a1c86e406af78314854aad4bb1d0af3f0cd
younguk2 <neostory1515@gmail.com>
2024-01-30 14:00:51 +0900

    refactor:"API 변경사항으로 ResumeController Mapping 변경"

--- src/main/java/univcapstone/employmentsite/controller/ResumeController.java
+++ src/main/java/univcapstone/employmentsite/controller/ResumeController.java
@@ -5,16 +5,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/jobhak.univ")
 public class ResumeController {
-    @RequestMapping(value = "/resume")
+
+    @RequestMapping(value = "/resume/write")
     @GetMapping
-    public String resume(){
+    public String resumeWrite(){
+        //자기소개서 첫화면 불러오기
+        return "";
+    }
+
+    @RequestMapping(value = "/resume/revise")
+    @GetMapping
+    public String resumeRevise(){
         //자기소개서 첫화면 불러오기
         return "";
     }
 
     @RequestMapping(value = "/resume/save")
     @PostMapping
-    public String save(@RequestParam("content") String content){
+    public String resumeSave(@RequestParam("content") String content){
         //자기소개서 보내기
         return "";
     }
