e7d4ac797838d1781dd30cc55158f517cfc86de2
younguk2 <neostory1515@gmail.com>
2024-03-08 14:38:48 +0900

    refactor:사람인 데이터 요청 데이터 변경 및 uri 변경

--- src/main/java/univcapstone/employmentsite/controller/BasicController.java
+++ src/main/java/univcapstone/employmentsite/controller/BasicController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import univcapstone.employmentsite.domain.User;
 import univcapstone.employmentsite.dto.JobResponseDto;
 import univcapstone.employmentsite.repository.UserRepository;
+import univcapstone.employmentsite.token.CustomUserDetails;
 import univcapstone.employmentsite.util.response.BasicResponse;
 import univcapstone.employmentsite.util.response.DefaultResponse;
 
@@ -29,12 +31,9 @@ public class BasicController {
     UserRepository userRepository;
 
     @GetMapping("/home/saramin")
-    public ResponseEntity<List<JobResponseDto.Job>> saramin(
-            @RequestBody Map<String, String> receiverMap
-    ) throws JsonProcessingException {
-        String keyword=receiverMap.get("keyword");
+    public ResponseEntity<List<JobResponseDto.Job>> saramin() throws JsonProcessingException {
         RestTemplate restTemplate=new RestTemplate();
-        String apiUrl = "https://oapi.saramin.co.kr/job-search?access-key=pEyjyJB3XnowAZP5ImZUuNbcGwGGDbUGQXQfdDZqhSFgPkBXKWq&keywords="+keyword; // 취업 사이트의 API URL
+        String apiUrl = "https://oapi.saramin.co.kr/job-search?access-key=pEyjyJB3XnowAZP5ImZUuNbcGwGGDbUGQXQfdDZqhSFgPkBXKWq&bbs_gb=1&sr=directhire&job_type=1,10&loc_cd=117000&sort=rc&start=0&count=12"; // 취업 사이트의 API URL
         ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
         log.info("응답된 내용 = {}",response);
         String responseBody=response.getBody();
@@ -45,16 +44,18 @@ public class BasicController {
         return ResponseEntity.ok()
                 .body(jobs);
     }
-    //닉네임 유저아이디
+
     @GetMapping("/home")
-    public ResponseEntity<? extends BasicResponse> home(Authentication authentication) {
-        log.info("current user login_id = {}", authentication.getName());
-        Optional<User> users=userRepository.findByLoginId(authentication.getName());
+    public ResponseEntity<? extends BasicResponse> home(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        log.info("current user = {}", customUserDetails.getUser());
+        Optional<User> users=userRepository.findByLoginId(customUserDetails.getUser().getLoginId());
         User user=users.get();
+
         if(user==null){
             log.info("유저를 찾을 수 없습니다.");
         }
-
 
         DefaultResponse<User> defaultResponse = DefaultResponse.<User>builder()
                 .code(HttpStatus.OK.value())
