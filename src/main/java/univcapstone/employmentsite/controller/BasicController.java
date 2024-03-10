f9481faf9e44061a7052cbc9e0619963df9af750
younguk2 <neostory1515@gmail.com>
2024-03-11 00:16:02 +0900

    feat:사람인 기업 img src 보내는 /home/saramin/href 추가

--- src/main/java/univcapstone/employmentsite/controller/BasicController.java
+++ src/main/java/univcapstone/employmentsite/controller/BasicController.java
@@ -11,16 +11,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import univcapstone.employmentsite.domain.Reply;
 import univcapstone.employmentsite.domain.User;
+import univcapstone.employmentsite.dto.ImageAPIHrefDto;
 import univcapstone.employmentsite.dto.JobResponseDto;
+import univcapstone.employmentsite.dto.ReplyToFrontDto;
 import univcapstone.employmentsite.repository.UserRepository;
 import univcapstone.employmentsite.token.CustomUserDetails;
 import univcapstone.employmentsite.util.response.BasicResponse;
 import univcapstone.employmentsite.util.response.DefaultResponse;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 @Slf4j
@@ -33,7 +43,7 @@ public class BasicController {
     @GetMapping("/home/saramin")
     public ResponseEntity<List<JobResponseDto.Job>> saramin() throws JsonProcessingException {
         RestTemplate restTemplate=new RestTemplate();
-        String apiUrl = "https://oapi.saramin.co.kr/job-search?access-key=pEyjyJB3XnowAZP5ImZUuNbcGwGGDbUGQXQfdDZqhSFgPkBXKWq&bbs_gb=1&sr=directhire&job_type=1,10&loc_cd=117000&sort=rc&start=0&count=12"; // 취업 사이트의 API URL
+        String apiUrl = "https://oapi.saramin.co.kr/job-search?access-key=pEyjyJB3XnowAZP5ImZUuNbcGwGGDbUGQXQfdDZqhSFgPkBXKWq&bbs_gb=1&sr=directhire&job_type=1,10&loc_cd=117000&sort=rc&start=0&count=12&fields=2023-03-11"; // 취업 사이트의 API URL
         ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
         log.info("응답된 내용 = {}",response);
         String responseBody=response.getBody();
@@ -41,8 +51,55 @@ public class BasicController {
         ObjectMapper mapper = new ObjectMapper();
         JobResponseDto jobResponse = mapper.readValue(responseBody, JobResponseDto.class);
         List<JobResponseDto.Job> jobs = jobResponse.getJobs().getJob();
+
+
         return ResponseEntity.ok()
                 .body(jobs);
+    }
+
+    @GetMapping("/home/saramin/href")
+    public ResponseEntity<List<String>> extractImages() throws JsonProcessingException {
+        RestTemplate restTemplate=new RestTemplate();
+        String apiUrl = "https://oapi.saramin.co.kr/job-search?access-key=pEyjyJB3XnowAZP5ImZUuNbcGwGGDbUGQXQfdDZqhSFgPkBXKWq&bbs_gb=1&sr=directhire&job_type=1,10&loc_cd=117000&sort=rc&start=0&count=12&fields=2023-03-11"; // 취업 사이트의 API URL
+        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+        log.info("응답된 내용 = {}",response);
+        String responseBody=response.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> hrefList=new ArrayList<>();
+        List<String> imageUrlList = new ArrayList<>();
+
+        if (responseBody != null) {
+            ImageAPIHrefDto imageAPIResponse = mapper.readValue(responseBody, ImageAPIHrefDto.class);
+
+            List<ImageAPIHrefDto.Job> jobs = imageAPIResponse.getJobs().getJob();
+            for (ImageAPIHrefDto.Job job : jobs) {
+                if (job.getCompany() != null && job.getCompany().getDetail() != null) {
+                    String href = job.getCompany().getDetail().getHref();
+                    try {
+                        Document document = Jsoup.connect(href).get();
+                        Elements elements = document.select("div.box_logo img");
+
+                        StringBuilder images = new StringBuilder();
+                        log.info("Elements = {}",elements);
+
+                        for (Element element : elements) {
+                            String imageUrl = element.attr("src");
+                            images.append("<img src='").append(imageUrl).append("' />");
+                        }
+
+                        imageUrlList.add(images.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    hrefList.add(href);
+                }
+            }
+        }
+        log.info("hef URL 데이터 = {}",hrefList);
+        log.info("이미지 URL 데이터 = {}",imageUrlList);
+        return ResponseEntity.ok()
+                .body(imageUrlList);
     }
 
     @GetMapping("/home")
