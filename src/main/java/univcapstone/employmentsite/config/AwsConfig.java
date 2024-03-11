45e2c832d32cdcd827578a642cacaf70236f3d5f
younguk2 <neostory1515@gmail.com>
2024-03-11 23:42:52 +0900

    feat:AWS S3 config, PictureController 사진 데이터 저장

---
+++ src/main/java/univcapstone/employmentsite/config/AwsConfig.java
@@ -0,0 +1,34 @@
+package univcapstone.employmentsite.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.regions.Regions;
+@Configuration
+@Slf4j
+public class AwsConfig {
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        return AmazonS3Client.builder()
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
--- src/main/java/univcapstone/employmentsite/controller/PictureController.java
+++ src/main/java/univcapstone/employmentsite/controller/PictureController.java
@@ -1,18 +1,31 @@
 package univcapstone.employmentsite.controller;
 
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import univcapstone.employmentsite.util.response.BasicResponse;
 import univcapstone.employmentsite.util.response.DefaultResponse;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Slf4j
 @RestController
 public class PictureController {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
     @GetMapping("/profile/male")
     public String malePicture(){
         //취업사진 남성용
@@ -69,9 +82,29 @@ public class PictureController {
         return "";
     }
 
-    @GetMapping("/profile/save")
-    public String save(){
-        //이미지 저장
-        return "";
+    @PostMapping("/profile/save")
+    public String save(
+            //@RequestBody MultipartFile file
+    ){
+        // 파일 경로 설정
+        String fileName = "tommy.jpg"; // 이미지 파일 이름
+        String filePath = fileName; // 이미지 파일이 컨트롤러와 같은 위치에 있다고 가정
+
+        try {
+            // S3에 업로드할 파일 객체 생성
+            File file = new File(filePath);
+
+            // S3에 업로드할 객체 메타데이터 생성
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.length());
+
+            // S3에 사진 업로드
+            amazonS3.putObject(new PutObjectRequest("jobhakdasik2000-bucket", fileName, file).withMetadata(metadata));
+
+            return "Photo uploaded to Amazon S3 successfully!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Failed to upload photo to Amazon S3!";
+        }
     }
 }
