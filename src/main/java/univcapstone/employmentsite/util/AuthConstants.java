da004bde8c86950560713fec7302134b9a7a1cd0
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-03-20 18:51:13 +0900

    chore: URI 수정 및 Configuration 수정

--- src/main/java/univcapstone/employmentsite/util/AuthConstants.java
+++ src/main/java/univcapstone/employmentsite/util/AuthConstants.java
@@ -6,6 +6,6 @@ public class AuthConstants {
     public static final String AUTH_HEADER = "Authorization";
     public static final String REFRESH_HEADER = "Refresh";
     public static final String BEARER_PREFIX = "Bearer ";
-    public static final long ACCESS_TOKEN_VALID_TIME = 1000 * 10;            // 30분
+    public static final long ACCESS_TOKEN_VALID_TIME = 1000 * 60 * 30;            // 30분
     public static final long REFRESH_TOKEN_VALID_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 }
