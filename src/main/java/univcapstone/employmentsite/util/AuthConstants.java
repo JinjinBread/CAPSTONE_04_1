d9aebc6f0a6acb540bef9b9a663fcab04cf340e2
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-05-26 01:47:24 +0900

    refactor: 토큰 유효 기간 상수 변경

--- src/main/java/univcapstone/employmentsite/util/AuthConstants.java
+++ src/main/java/univcapstone/employmentsite/util/AuthConstants.java
@@ -8,8 +8,8 @@ public class AuthConstants {
     public static final String REFRESH_COOKIE_NAME = "refreshToken";
     public static final String BEARER_PREFIX = "Bearer ";
     public static final int OTP_TTL = 30; //30분
-    public static final long ACCESS_TOKEN_VALID_TIME = 1000 * 30 * 1;            // 30분
-    public static final long REFRESH_TOKEN_VALID_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일 (초 * 분 * 시간 * 일)
+    public static final long ACCESS_TOKEN_VALID_TIME = 1000 * 60 * 30;            // 30분
+    public static final long REFRESH_TOKEN_VALID_TIME = 60 * 60 * 24 * 7;  // 7일 (초 * 분 * 시간 * 일)
 
     public static final String OAUTH_REDIRECT_URI = "https://localhost:3000/login/callback";
 }
